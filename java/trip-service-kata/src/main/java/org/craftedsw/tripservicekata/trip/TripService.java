b5cfc349739000cfdcb742152e58346dc985dea4
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-14 10:07:14 +0000

    Inline method, flip if and extract guard clause

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,6 +1,5 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,17 +11,17 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		User loggedUser = loggedUser();
-		if (userIsLoggedIn(loggedUser)) {
-			if (isFriendsWith(loggedUser, user)) {
-				return findTripsByUser(user);
-			}
-			return Collections.emptyList();
+		ensureUserIsLoggedIn(loggedUser);
+		if (isFriendsWith(loggedUser, user)) {
+			return findTripsByUser(user);
 		}
-		throw new UserNotLoggedInException();
+		return Collections.emptyList();
 	}
 
-	private boolean userIsLoggedIn(User loggedUser) {
-		return loggedUser != null;
+	private void ensureUserIsLoggedIn(User loggedUser) {
+		if (loggedUser == null) {
+			throw new UserNotLoggedInException();
+		}
 	}
 
 	private boolean isFriendsWith(User loggedUser, User user) {
